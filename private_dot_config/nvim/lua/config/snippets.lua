8eb43389d5520c331555d15db96a9fb39ef55539
Jaeyeol Lee <rijgndqw012@gmail.com>
2022-10-10 01:09:14 +0900

    (nvim) Setup kotlin related snippets

---
+++ private_dot_config/nvim/lua/config/snippets.lua
@@ -0,0 +1,40 @@
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+keymap("i", "<c-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
+keymap("s", "<c-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
+keymap("i", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
+keymap("s", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
+
+local ls = require("luasnip")
+-- some shorthands...
+local snip = ls.snippet
+local node = ls.snippet_node
+local text = ls.text_node
+local insert = ls.insert_node
+local func = ls.function_node
+local choice = ls.choice_node
+local dynamicn = ls.dynamic_node
+
+local date = function() return {os.date('%Y-%m-%d')} end
+
+ls.add_snippets(nil, {
+    all = {
+        snip({
+            trig = "date",
+            namr = "Date",
+            dscr = "Date in the form of YYYY-MM-DD",
+        }, {
+            func(date, {}),
+        }),
+    },
+
+		kotlin = {
+				snip({
+						trig = "main-ps",
+						namr = "main function for PS",
+						dscr = "main function definition for Problem Solving",
+				}, {
+						text("fun main(args: Array<String>) = with(Scanner(System.`in`)) {}")
+				})
+		}
+})

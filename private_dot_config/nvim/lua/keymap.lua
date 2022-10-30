9a7c88b2dca996ecc5838bb9131a1af0ca8ad7c6
Jaeyeol Lee <rijgndqw012@gmail.com>
2022-10-30 13:49:17 +0900

    (nvim) Add keymaps related to Telekasten

--- private_dot_config/nvim/lua/keymap.lua
+++ private_dot_config/nvim/lua/keymap.lua
@@ -8,3 +8,22 @@ map('n', '<leader>n', ':NvimTreeFindFile<CR>', default_opts) -- search file
 
 -- Testing
 map('n', '<leader>t', ':TestNearest<CR>', default_opts)
+
+-- Telekasten
+map('n', "<leader>zf", ":lua require('telekasten').find_notes()<CR>", default_opts)
+map('n', "<leader>zd", ":lua require('telekasten').find_daily_notes()<CR>", default_opts)
+map('n', "<leader>zz", ":lua require('telekasten').follow_link()<CR>", default_opts)
+map('n', "<leader>zT", ":lua require('telekasten').goto_today()<CR>", default_opts)
+map('n', "<leader>zW", ":lua require('telekasten').goto_thisweek()<CR>", default_opts)
+map('n', "<leader>zn", ":lua require('telekasten').new_note()<CR>", default_opts)
+map('n', "<leader>zc", ":lua require('telekasten').show_calendar()<CR>", default_opts)
+map('n', "<leader>zC", ":CalendarT<CR>", default_opts)
+map('n', "<leader>zt", ":lua require('telekasten').toggle_todo()<CR>", default_opts)
+map('n', "<leader>zb", ":lua require('telekasten').show_backlinks()<CR>", default_opts)
+
+map('n', "<leader>z", ":lua require('telekasten').panel()<CR>", default_opts)
+
+map('i', "<leader>[", "<cmd>:lua require('telekasten').insert_link({ i=true })<CR>", default_opts)
+map('i', "<leader>zt", "<cmd>:lua require('telekasten').toggle_todo({ i=true })<CR>", default_opts)
+map('i', "<leader>#", "<cmd>lua require('telekasten').show_tags({i = true})<CR>", default_opts)
+

df24bf2ba6d4ecefd59cbcbe6e0d4e878475cb3a
Jaeyeol Lee <rijgndqw012@gmail.com>
2023-05-13 16:47:37 +0900

    (nvim) Add keymap for `switch_to_hangeul` function (visual mode)
    closes #7

--- private_dot_config/nvim/lua/keymap.lua
+++ private_dot_config/nvim/lua/keymap.lua
@@ -87,3 +87,12 @@ map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
 map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
 map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
 map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+-- Utility functions for Note taking
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {'markdown'},
+  group = augroup,
+  desc = 'Only works on markdown files',
+  callback = function(event)
+    map("v", "<leader>th", ":lua require('utilities.note_taking').switch_to_hangeul()<CR>", opts)
+  end
+})

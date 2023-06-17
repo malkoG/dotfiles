9a72860d49150ba198a7370b499b88de98cd8987
Jaeyeol Lee <rijgndqw012@gmail.com>
2023-06-17 09:13:09 +0900

    (nvim) Disable shade.nvim

--- private_dot_config/nvim/lua/plugins/packer.lua
+++ private_dot_config/nvim/lua/plugins/packer.lua
@@ -138,19 +138,19 @@ return require('packer').startup(function(use)
       })
     end
   })
-  use({
-    'sunjon/shade.nvim',
-    config = function()
-      require'shade'.setup({
-        overlay_opacity = 50,
-        opacity_step = 1,
-        keys = {
-          brightness_up    = '<C-Up>',
-          brightness_down  = '<C-Down>',
-        }
-      })
-    end
-  })
+  -- use({
+  --  'sunjon/shade.nvim',
+  --  config = function()
+  --    require'shade'.setup({
+  --      overlay_opacity = 0,
+  --      opacity_step = 1,
+  --      keys = {
+  --        brightness_up    = '<C-Up>',
+  --        brightness_down  = '<C-Down>',
+  --      }
+  --    })
+  --  end
+  -- })
 
   -- Git utility
   use { "APZelos/blamer.nvim" }

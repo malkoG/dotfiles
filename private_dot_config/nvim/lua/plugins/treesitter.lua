8278379782870cf11c763b17c2465788aa69abf0
malkoG <rijgndqw012@gmail.com>
2023-07-17 12:23:33 +0900

    (nvim) Enable syntax highlighting based on nvim-treesitter

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -33,6 +33,7 @@ require'nvim-treesitter.configs'.setup {
   auto_install = true,
 
   highlight = {
+    enable = true,
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.

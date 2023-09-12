ac60c4acc9195c145cf55393412ce1d19f905e73
malkoG <rijgndqw012@gmail.com>
2023-09-12 21:14:06 +0900

    (nvim) Add scheduler script to be automatically get notified for user-defined timeslot

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -7,6 +7,21 @@ require('plugins/blamer')
 
 require('ftplugins.purescript')
 
+local scheduler = require('utilities.scheduler')
+
+vim.g.slotted_schedules = {
+  { message = "Beginning Slot 1", hour = 10, minute = 00 },
+  { message = "Finishing Slot 1", hour = 13, minute = 30 },
+
+  { message = "Beginning Slot 2", hour = 15, minute = 00 },
+  { message = "Finishing Slot 2", hour = 18, minute = 00 },
+
+  { message = "Beginning Slot 3", hour = 19, minute = 30 },
+  { message = "Finishing Slot 3", hour = 23, minute = 00 },
+}
+
+scheduler.notify_schedule()
+
 require('coc/init')
 require('keymap')
 
---
+++ private_dot_config/nvim/lua/utilities/scheduler.lua
@@ -0,0 +1,34 @@
+local M = {}
+
+M.notify_schedule = function()
+  -- This function runs timer and notifies on specified time
+  --
+  -- For example,
+  --   13:00:00 (Alarm)
+  --   13:00:01 (Doesn't alarm)
+  --   13:59:59 (Doesn't alarm)
+  --   14:00:00 (Alarm)
+  --
+  -- And We can manage schedules using tables
+  -- For example,
+  --   local schedules = {
+  --     { message = "Starting phase 1", hour = 13, minute = 30 },
+  --     { message = "Starting phase 2", hour = 16, minute = 30 },
+  --     { message = "Starting phase 3", hour = 19, minute = 30 },
+  --   }
+
+  local schedules = vim.g.slotted_schedules or {}
+
+  local function notify()
+    local now = os.date("*t")
+    for _, schedule in ipairs(schedules) do
+      if now.hour == schedule.hour and now.min == schedule.minute and now.sec <= 2 then
+        vim.notify(schedule.message, vim.log.levels.INFO)
+      end
+    end
+  end
+
+  vim.defer_fn(notify, 1000)
+end
+
+return M

d29a407893f35859ca50c4bf1d37d39afdf165b4
malkoG <rijgndqw012@gmail.com>
2023-09-13 13:16:25 +0900

    (nvim) Replace ChatGPT.nvim to neoai.nvim

--- private_dot_config/nvim/lua/plugins/packer.lua
+++ private_dot_config/nvim/lua/plugins/packer.lua
@@ -80,20 +80,93 @@ return require('packer').startup(function(use)
   -- Productivity
   use { 'wakatime/vim-wakatime' }
   use { 'github/copilot.vim' }
-  use {
-    "jackMort/ChatGPT.nvim",
+  -- use {
+  --   "jackMort/ChatGPT.nvim",
+  --   config = function()
+  --     require("chatgpt").setup({
+  --       -- optional configuration
+  --     })
+  --   end,
+  --   requires = {
+  --     "MunifTanjim/nui.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-telescope/telescope.nvim",
+  --   }
+  -- }
+  use ({
+    "Bryley/neoai.nvim",
+    requires = { "MunifTanjim/nui.nvim" },
+    cmd = {
+      "NeoAI",
+      "NeoAIOpen",
+      "NeoAIClose",
+      "NeoAIToggle",
+      "NeoAIContext",
+      "NeoAIContextOpen",
+      "NeoAIContextClose",
+      "NeoAIInject",
+      "NeoAIInjectCode",
+      "NeoAIInjectContext",
+      "NeoAIInjectContextCode",
+    },
     config = function()
-      require("chatgpt").setup({
-        -- optional configuration
+      require("neoai").setup({
+        -- Below are the default options, feel free to override what you would like changed
+        ui = {
+            output_popup_text = "NeoAI",
+            input_popup_text = "Prompt",
+            width = 30, -- As percentage eg. 30%
+            output_popup_height = 80, -- As percentage eg. 80%
+            submit = "<Enter>", -- Key binding to submit the prompt
+        },
+        models = {
+            {
+                name = "openai",
+                model = "gpt-3.5-turbo",
+                params = nil,
+            },
+        },
+        register_output = {
+            ["g"] = function(output)
+                return output
+            end,
+            ["c"] = require("neoai.utils").extract_code_snippets,
+        },
+        inject = {
+            cutoff_width = 75,
+        },
+        prompts = {
+            context_prompt = function(context)
+                return "Hey, I'd like to provide some context for future "
+                    .. "messages. Here is the code/text that I want to refer "
+                    .. "to in our upcoming conversations:\n\n"
+                    .. context
+            end,
+        },
+        mappings = {
+            ["select_up"] = "<C-k>",
+            ["select_down"] = "<C-j>",
+        },
+        open_ai = {
+            api_key = {
+                env = "OPENAI_API_KEY",
+                value = nil,
+                -- `get` is is a function that retrieves an API key, can be used to override the default method.
+                -- get = function() ... end
+
+                -- Here is some code for a function that retrieves an API key. You can use it with
+                -- the Linux 'pass' application.
+                -- get = function()
+                --     local key = vim.fn.system("pass show openai/mytestkey")
+                --     key = string.gsub(key, "\n", "")
+                --     return key
+                -- end,
+            },
+        },
       })
     end,
-    requires = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "~/neovim-plugin/vault.nvim"
-    }
-  }
+  })
+
   use {
     'phaazon/mind.nvim',
     branch = 'v2.2',

fde8eda5f28f679a8afb597ee46d335318bd2aa2
malkoG <rijgndqw012@gmail.com>
2023-11-02 16:41:21 +0900

    (wezterm) Update font in wezterm configuration
    Changed the font in the wezterm configuration file to '셈틀체'

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local actions = require("actions")
 local keymaps = require("keymaps")
 
 return {
-	font = wezterm.font_with_fallback({'JetBrains Mono', 'NanumBarunGothic'}),
+	font = wezterm.font_with_fallback({'셈틀체', 'NanumBarunGothic'}),
 	font_size = 14.0,
 	window_background_opacity = 0.9,
 	keys = keymaps,

7e08278b64dfc582055c8b91895a97c12b3dcdc5
malkoG <rijgndqw012@gmail.com>
2023-12-14 22:47:02 +0900

    (nvim) Update Neovim settings with Korean langmap configuration
    * See https://github.com/johngrib/simple_vim_guide/blob/master/md/with_korean.md

--- private_dot_config/nvim/lua/settings.lua
+++ private_dot_config/nvim/lua/settings.lua
@@ -9,6 +9,7 @@ opt.relativenumber = true
 opt.shiftwidth = 4
 opt.tabstop = 4
 opt.colorcolumn = "120"
+opt.langmap = "ㅁa,ㅠb,ㅊc,ㅇd,ㄷe,ㄹf,ㅎg,ㅗh,ㅑi,ㅓj,ㅏk,ㅣl,ㅡm,ㅜn,ㅐo,ㅔp,ㅂq,ㄱr,ㄴs,ㅅt,ㅕu,ㅍv,ㅈw,ㅌx,ㅛy,ㅋz"
 
 -- git-gutter settings
 g.gitgutter_set_sign_backgrounds = 1

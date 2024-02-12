fa6e619c814526c5b69e42d7696ba2e8ba22f7a3
malkoG <rijgndqw012@gmail.com>
2024-02-12 17:07:46 +0900

    (nvim) Treat dash(-) and underscore(_) as a separator, not word
    * Configure Neovim to recognize dash and underscore as non-word characters,
      improving text navigation and editing.

--- private_dot_config/nvim/lua/settings.lua
+++ private_dot_config/nvim/lua/settings.lua
@@ -11,6 +11,12 @@ opt.tabstop = 4
 opt.colorcolumn = "120"
 opt.langmap = "ㅁa,ㅠb,ㅊc,ㅇd,ㄷe,ㄹf,ㅎg,ㅗh,ㅑi,ㅓj,ㅏk,ㅣl,ㅡm,ㅜn,ㅐo,ㅔp,ㅂq,ㄱr,ㄴs,ㅅt,ㅕu,ㅍv,ㅈw,ㅌx,ㅛy,ㅋz"
 
+-- Treating dash(-) and underscore(_) as a separator, not words
+--
+-- ex. rub|y_on_rails -- (typing ciw) --> *_on_rails
+opt.iskeyword:remove("-")
+opt.iskeyword:remove("_")
+
 -- git-gutter settings
 g.gitgutter_set_sign_backgrounds = 1
 

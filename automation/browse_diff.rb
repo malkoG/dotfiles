e32c89dd2e52f499976769357d980b9c02347143
malkoG <rijgndqw012@gmail.com>
2024-02-28 19:15:28 +0900

    Add interactive git log browser script

---
+++ automation/browse_diff.rb
@@ -0,0 +1,11 @@
+git_logs = `git log --oneline | gum choose --limit 100`
+
+# See https://abhij.it/ruby-difference-between-system-exec-and-backticks/
+git_logs.each_line do |line|
+  commit_hash, *_ = line.split
+  system("git show #{commit_hash}")
+  puts("=====")
+  puts("Press ENTER key to CONTINUE")
+  puts("=====")
+  gets
+end

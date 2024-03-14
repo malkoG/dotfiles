67e6adba044d277a29fd85cb32c5589049f9ae46
malkoG <rijgndqw012@gmail.com>
2024-03-14 16:43:46 +0900

    (nvim) Disable treesitter highlighter for markdown files

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -40,7 +40,7 @@ require'nvim-treesitter.configs'.setup {
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
-    disable = { "typescript" },
+    disable = { "typescript", "markdown" },
   },
 }
 

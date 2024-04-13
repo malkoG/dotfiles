ae58cb0300ea841e2c60fdd8352e554a36157f50
malkoG <rijgndqw012@gmail.com>
2024-04-13 23:11:43 +0900

    (nvim) Adjust default behaviour of vertical split
    * This option is essential to oil.nvim

--- private_dot_config/nvim/lua/settings.lua
+++ private_dot_config/nvim/lua/settings.lua
@@ -10,6 +10,7 @@ opt.shiftwidth = 4
 opt.tabstop = 4
 opt.colorcolumn = "120"
 opt.langmap = "ㅁa,ㅠb,ㅊc,ㅇd,ㄷe,ㄹf,ㅎg,ㅗh,ㅑi,ㅓj,ㅏk,ㅣl,ㅡm,ㅜn,ㅐo,ㅔp,ㅂq,ㄱr,ㄴs,ㅅt,ㅕu,ㅍv,ㅈw,ㅌx,ㅛy,ㅋz"
+opt.splitright = true
 
 -- Treating dash(-) and underscore(_) as a separator, not words
 --

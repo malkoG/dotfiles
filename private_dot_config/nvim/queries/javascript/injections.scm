0b66a40bde9d8a76460201a4f5b2d4628bd2ca3e
malkoG <rijgndqw012@gmail.com>
2024-06-20 18:24:46 +0900

    (nvim) Add treesitter injection query for vue.js

---
+++ private_dot_config/nvim/queries/javascript/injections.scm
@@ -0,0 +1,12 @@
+
+(object
+  (pair
+	(property_identifier) @key
+	(#eq? @key "template")
+	(template_string
+		(string_fragment) @injection.content 
+		(#set! injection.language "html")
+		(#set! injection.combined)
+		)))
+
+

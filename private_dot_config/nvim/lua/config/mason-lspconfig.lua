4daaf328805371522a7d5c2fdf47d2d4f8c5bcce
malkoG <rijgndqw012@gmail.com>
2024-07-21 20:07:50 +0900

    (nvim) Setup OCaml development environment

--- private_dot_config/nvim/lua/config/mason-lspconfig.lua
+++ private_dot_config/nvim/lua/config/mason-lspconfig.lua
@@ -48,6 +48,7 @@ M.setup = function()
       "terraformls",
       "jinja_lsp",
       "volar",
+      "ocamllsp",
     },
   })
 
@@ -124,6 +125,7 @@ M.setup = function()
   }
   require("lspconfig")["terraformls"].setup{}
   require("lspconfig")["jinja_lsp"].setup{}
+  require("lspconfig")["ocamllsp"].setup{}
 
   if ruby_lsp_local == "true" then
     -- require("lspconfig").solargraph.setup {

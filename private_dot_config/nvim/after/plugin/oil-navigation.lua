01d6cf9cb96a4e8c10539b67b78c7e498c1017a5
malkoG <rijgndqw012@gmail.com>
2024-12-03 00:46:02 +0900

    (nvim) Add oil-navigation - open terminal in current directory with custom commands

---
+++ private_dot_config/nvim/after/plugin/oil-navigation.lua
@@ -0,0 +1,36 @@
+local oil = require("oil")
+
+local default_opts = { noremap = true, silent = true }
+local opts = default_opts
+
+local term_on_current_dir = function()
+  local current_dir = oil.get_current_dir(0)
+  print(current_dir);
+  if current_dir then
+    vim.api.nvim_command("vsplit | term") -- Open terminal in a horizontal split
+    vim.fn.chansend(vim.b.terminal_job_id, "cd " .. current_dir .. "\n") -- Send the `cd` command to the terminal
+
+  else
+    print("Error: Unable to get the current directory.")
+  end
+end
+
+local aider_term_on_current_dir = function()
+	term_on_current_dir()
+	vim.fn.chansend(vim.b.terminal_job_id, "aider --subtree-only\n")
+end
+
+vim.api.nvim_create_autocmd(
+  'FileType',
+  {
+    pattern = 'oil',
+    desc = 'Open current directory in terminal mode',
+    callback = function(event)
+      vim.api.nvim_create_user_command("TermOnCurrentDir", term_on_current_dir, {})
+			vim.api.nvim_create_user_command("AiderTermOnCurrentDir", aider_term_on_current_dir, {})
+
+			vim.keymap.set("n", "<leader>tt", "<Cmd>TermOnCurrentDir<CR>", opts)
+			vim.keymap.set("n", "<leader>ta", "<Cmd>AiderTermOnCurrentDir<CR>", opts)
+    end
+  }
+)

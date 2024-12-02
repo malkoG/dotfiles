c98dbe12771a5f7b1d7b0764c593746aee157bbd
malkoG <rijgndqw012@gmail.com>
2024-12-03 00:47:34 +0900

    (nvim) Add mastodon dependency

--- private_dot_config/nvim/lua/deps/productivity.lua
+++ private_dot_config/nvim/lua/deps/productivity.lua
@@ -3,5 +3,16 @@ return {
   {
     'renerocksai/telekasten.nvim',
     dependencies = { 'renerocksai/calendar-vim' },
+  },
+  {
+    "kode-team/mastodon.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "rcarriga/nvim-notify",
+      "kkharji/sqlite.lua",
+    },
+    config = function()
+      require("mastodon").setup()
+    end
   }
 }

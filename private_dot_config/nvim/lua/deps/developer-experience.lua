6192aa2d5171ff31a066815032515f473dca7b75
malkoG <rijgndqw012@gmail.com>
2025-05-03 21:52:14 +0900

    (nvim) Enable Trouble.nvim keymap

--- private_dot_config/nvim/lua/deps/developer-experience.lua
+++ private_dot_config/nvim/lua/deps/developer-experience.lua
@@ -198,6 +198,40 @@ return {
   { "airblade/vim-gitgutter" },
   {
     "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
   },
   { 'brenoprata10/nvim-highlight-colors' },
   { "malkoG/vim-test", branch = 'master' },
--- private_dot_config/nvim/lua/keymap.lua
+++ private_dot_config/nvim/lua/keymap.lua
@@ -158,9 +158,3 @@ vim.api.nvim_create_autocmd('FileType', {
   end
 })
 
-vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
-vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
-vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
-vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
-vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
-vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)

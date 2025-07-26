6f580ded53bfca1700b8a74d1a133d37c76e0515
malkoG <rijgndqw012@gmail.com>
2025-07-26 10:43:34 +0900

    (nvim) Remove avente.nvim dependency

--- private_dot_config/nvim/lua/deps/developer-experience.lua
+++ private_dot_config/nvim/lua/deps/developer-experience.lua
@@ -84,51 +84,15 @@ return {
     end
   },
   {
-    "yetone/avante.nvim",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      'echasnovski/mini.icons',
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        "MeanderingProgrammer/render-markdown.nvim",
-        after = { 
-          "nvim-treesitter",
-          'echasnovski/mini.icons'
-        },
-        opts = {
-          file_types = { "Avante", "markdown" },
-        },
-        ft = { "Avante", "markdown" },
-      }
+    "MeanderingProgrammer/render-markdown.nvim",
+    after = { 
+      "nvim-treesitter",
+      'echasnovski/mini.icons'
     },
-    run = "make",
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- set this if you want to always pull the latest change
     opts = {
-      provider = "copilot",
-      auto_suggestion_provider = "copilot",
+      file_types = { "Avante", "markdown" },
     },
+    ft = { "Avante", "markdown" },
   },
   { "gpanders/editorconfig.nvim" },
   { "echasnovski/mini.files" },

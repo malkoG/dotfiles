486ca4a33566980fb77a03e9b06f7f73014be341
malkoG <rijgndqw012@gmail.com>
2025-12-03 06:53:06 +0900

    Remove not using configs

--- private_dot_config/sketchybar/items/executable_filler.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item filler.left left \
-           --set filler.left padding_left=520
-
-sketchybar --add item filler.right right \
-           --set filler.right padding_right=210
--- private_dot_config/sketchybar/items/executable_wifi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item net right                  \
-           --set net script="$PLUGIN_DIR/wifi.sh" \
-                     updates=on                  \
-                     padding_right=0             \
-                     icon.padding_right=6        \
-           --subscribe net wifi_change

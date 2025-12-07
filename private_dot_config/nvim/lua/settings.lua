2d033f18e23d3229074c4fe57ea7643290fc04b4
malkoG <rijgndqw012@gmail.com>
2025-12-07 21:36:43 +0900

    (nvim) Update colorcolumn setting to allow multiple column markers

--- private_dot_config/nvim/lua/settings.lua
+++ private_dot_config/nvim/lua/settings.lua
@@ -8,7 +8,7 @@ opt.number = true
 opt.relativenumber = true
 opt.shiftwidth = 4
 opt.tabstop = 4
-opt.colorcolumn = "120"
+opt.colorcolumn = { 79, 99, 119 }
 opt.langmap = "ㅁa,ㅠb,ㅊc,ㅇd,ㄷe,ㄹf,ㅎg,ㅗh,ㅑi,ㅓj,ㅏk,ㅣl,ㅡm,ㅜn,ㅐo,ㅔp,ㅂq,ㄱr,ㄴs,ㅅt,ㅕu,ㅍv,ㅈw,ㅌx,ㅛy,ㅋz"
 opt.splitright = true
 

ac9082820f1611bf9730352d5394775b86458cd4
malkoG <rijgndqw012@gmail.com>
2026-03-02 13:38:43 +0900

    (nvim) Add outliner.nvim

--- private_dot_config/nvim/lua/deps/developer-experience.lua
+++ private_dot_config/nvim/lua/deps/developer-experience.lua
@@ -1,6 +1,17 @@
 return {
   { 'nvim-treesitter/nvim-treesitter' },
   {
+    "hedyhli/outline.nvim",
+    lazy = true,
+    cmd = { "Outline", "OutlineOpen" },
+    keys = { -- Example mapping to toggle outline
+      { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+    },
+    opts = {
+      -- Your setup opts here
+    },
+  },
+  {
     "folke/snacks.nvim",
     opts = {
       picker = {

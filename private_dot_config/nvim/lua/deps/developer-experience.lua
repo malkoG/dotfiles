542e673756444f0b2bccbc3a858deca04216bacb
malkoG <rijgndqw012@gmail.com>
2026-03-02 13:42:35 +0900

    (nvim) Update keymap for snack.picker

--- private_dot_config/nvim/lua/deps/developer-experience.lua
+++ private_dot_config/nvim/lua/deps/developer-experience.lua
@@ -40,7 +40,7 @@ return {
       -- git
       { "<leader>fB", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
       { "<leader>fl", function() Snacks.picker.git_log() end, desc = "Git Log" },
-      { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
+      { "<leader>fL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
       { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
       { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
       { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
